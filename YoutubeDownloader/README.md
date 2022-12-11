1674ecd2b07a9ef7a51b522d4b038cb596e026c5
Edmond Bosco <edmond.bosco@bliksund.com>
2022-12-11 15:30:40 +0100

    first commit

---
+++ YoutubeDownloader/README.md
@@ -0,0 +1,12 @@
+This code creates a simple GUI window with a label and an entry field where you can enter the URL of the YouTube video you want to download. It also has a dropdown menu that allows you to select the desired resolution of the video. When you click the "Download Video" button, the code uses the ``pytube`` library to download the video in the selected resolution. The video will be saved to the current directory.
+
+You will need to install the ``pytube`` and ``tkinter`` libraries in order to use this code. You can install these libraries using the ``pip`` package manager. For example, to install ``pytube``, you can use the following command:
+
+``pip install pytube``
+
+You can then use this command to install tkinter:
+
+``pip install tkinter``
+
+
+
---
+++ YoutubeDownloader/Ytubedownloader.py
@@ -0,0 +1,42 @@
+import tkinter as tk
+from pytube import YouTube
+
+# Create the GUI window
+root = tk.Tk()
+root.title("YouTube Video Downloader")
+
+# Create a label and an entry field to enter the URL of the YouTube video
+url_label = tk.Label(root, text="Enter the URL of the YouTube video:")
+url_label.pack()
+url_entry = tk.Entry(root)
+url_entry.pack()
+
+# Create a function to download the video
+def download_video():
+    # Get the URL from the entry field
+    url = url_entry.get()
+    
+    # Use pytube to download the video
+    yt = YouTube(url)
+    
+    # Get the selected resolution
+    resolution = resolution_var.get()
+    
+    # Download the video in the selected resolution
+    video = yt.streams('mp4', resolution)
+    video.download('C:\\Users\\edmon\\Downloads')
+
+# Create a variable to store the selected resolution
+resolution_var = tk.StringVar()
+resolution_var.set("4K")
+
+# Create a dropdown menu to select the resolution
+resolution_menu = tk.OptionMenu(root, resolution_var, "4K", "1080p", "720p", "480p", "360p")
+resolution_menu.pack()
+
+# Create a button to trigger the download
+download_button = tk.Button(root, text="Download Video", command=download_video)
+download_button.pack()
+
+# Run the GUI
+root.mainloop()
